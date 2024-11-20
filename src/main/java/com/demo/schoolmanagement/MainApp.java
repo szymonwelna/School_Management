9e51f6c385cdf0a8a2711748e8c5d81eb9a1b7bc
welna <szymonwelna04@gmail.com>
2024-11-20 09:17:08 +0100

    Converted ListsHolder to singleton so it is a global object for storing data
    Added temporary function for initializing students list in listholder, it will be used for accessing students list from StudentsWindow

--- src/main/java/com/demo/schoolmanagement/MainApp.java
+++ src/main/java/com/demo/schoolmanagement/MainApp.java
@@ -1,5 +1,7 @@
 package com.demo.schoolmanagement;
 
+import com.demo.schoolmanagement.models.ListsHolder;
+import com.demo.schoolmanagement.models.Student;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -27,4 +29,32 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+
+    // Tymczasowa funkcja służąca wypełnieniu listy studentów
+    public void initStudentsList() {
+        ListsHolder listsHolder = ListsHolder.getInstance();
+
+        String[] studentsData = {
+                "Jan Kowalski", "Anna Nowak", "Piotr Zieliński", "Katarzyna Wiśniewska",
+                "Tomasz Wójcik", "Agnieszka Kaczmarek", "Michał Mazur", "Monika Adamczyk",
+                "Paweł Dąbrowski", "Ewa Jabłońska", "Krzysztof Zawadzki", "Maria Wojciechowska",
+                "Grzegorz Kamiński", "Joanna Walczak", "Łukasz Sikora", "Sylwia Baran",
+                "Damian Sadowski", "Natalia Dudek", "Adrian Walczak", "Iwona Kwiatkowska",
+                "Rafał Głowacki", "Dominika Zielińska", "Jakub Wojtaszek", "Karolina Nowicka",
+                "Mateusz Król", "Aleksandra Bąk", "Sebastian Majewski", "Patrycja Malinowska",
+                "Grzegorz Brzęczyszczykiewicz"
+        };
+
+        for (String studentData : studentsData) {
+            String[] nameParts = studentData.split(" ");
+            String firstName = nameParts[0];
+            String lastName = nameParts[1];
+
+            // Tworzenie studenta i dodawanie do globalnego ListsHolder
+            Student student = new Student(listsHolder.getLastId(), firstName, lastName);
+            listsHolder.addStudentToList(student);
+        }
+    }
+
 }
--- src/main/java/com/demo/schoolmanagement/models/ListsHolder.java
+++ src/main/java/com/demo/schoolmanagement/models/ListsHolder.java
@@ -2,24 +2,51 @@ package com.demo.schoolmanagement.models;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ListsHolder {
 
-    private List<User> users;
-    private Map<Integer, Student> students;
-    private Map<Integer, Teacher> teachers;
-    int lecturesNum;
-    private Map<Integer, Lecture> lectures;
+    // Statyczna instancja singletona
+    private static final ListsHolder instance = new ListsHolder();
 
+    // Prywatne pola
+    private int lastId = 0;
+    private List<User> users;
+    private Map<Integer, Student> students = new ConcurrentHashMap<>();
+    private Map<Integer, Teacher> teachers = new ConcurrentHashMap<>();
+    private int lecturesNum = 0;
+    private Map<Integer, Lecture> lectures = new ConcurrentHashMap<>();
+
+    // Prywatny konstruktor, by zapobiec tworzeniu innych instancji
+    private ListsHolder() {}
+
+    // Publiczny dostęp do singletona
+    public static ListsHolder getInstance() {
+        return instance;
+    }
+
+    // Metoda dodawania studenta
     public void addStudentToList(Student student) {
         students.put(student.getId(), student);
-    }
-    public void addTeacherToList(Teacher teacher) {
-        teachers.put(teacher.getId(), teacher);
-    }
-    public void addLectureToList(Lecture lecture) {
-        lectures.put(lecturesNum++, lecture);
+        lastId++;
     }
 
-    // Loading functions to be added
+    // Metoda dodawania nauczyciela
+    public void addTeacherToList(Teacher teacher) {
+        teachers.put(teacher.getId(), teacher);
+        lastId++;
+    }
+
+    // Metoda dodawania wykładu
+    public void addLectureToList(Lecture lecture) {
+        lectures.put(lecturesNum++, lecture);
+        lastId++;
+    }
+
+    // Getter do pobierania ostatniego ID
+    public int getLastId() {
+        return lastId;
+    }
+
+    // Pozostałe metody (np. do ładowania danych, uzyskiwania list) można dodać tutaj
 }
